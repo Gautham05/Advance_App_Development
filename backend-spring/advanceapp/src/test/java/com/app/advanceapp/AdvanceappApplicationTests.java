98a8acaf40081340c404072cca9a7e7632b62cda
Gautham R K <120078927+Gautham05@users.noreply.github.com>
2024-03-26 10:38:39 +0530

    Add files via upload

---
+++ backend-spring/advanceapp/src/test/java/com/app/advanceapp/AdvanceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.advanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
